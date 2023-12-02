31472d444c39dea5491ac7b6e3e2c441720f9fd3
gunyu1019 <gunyu1019@gmail.com>
2023-12-02 18:30:01 +0900

    [Docs] Add LICENCE and README

--- README.md
+++ README.md
@@ -1,33 +1,46 @@
-# async-client-decorator
+ # async-client-decorator
 
 Using `@decorator` to esaily request an HTTP Client<br/>
+This framework based on [aiohttp](https://github.com/aio-libs/aiohttp)'s http client framework.<br/>
 
-## Example
+Use Union Type to describe the elements required in an HTTP request.
 
+
+## Installation
+**Python 3.10 or higher is required.**
+
+```pip
+pip install async_client_decorator
+```
+
+## Quick Example
+
+An example is the API provided by the [BUS API](https://github.com/gunyu1019/trafficAPI).
 ```python
 import asyncio
 import aiohttp
-from async_client_decorator import request, Requestable, Header
+from async_client_decorator import request, Session, Query
 
 loop = asyncio.get_event_loop()
 
-class A(Requestable):
-    def __init__(self, loop):
-        super().__init__("https://base_url", loop)
+class BusAPI(Session):
+    def __init__(self, loop: asyncio.AbstractEventLoop):
+        super().__init__("https://api.yhs.kr", loop)
 
-    @request("GET", "/status")
-    async def status(
+    @request("GET", "/bus/station")
+    async def station_search_with_query(
                 self,
                 response: aiohttp.ClientResponse,
-                authorization: Header | str
+                name: Query | str
     ):
         return await response.json() 
-    
+
+
 async def main():
-    session = A(loop)
-    data = await session.status(authorization="KEY")
-    print(data)
-    await session.close()
+    async with BusAPI(loop) as client:
+        response = await client.station_search_with_query(name="bus-station-name")
+        data = await response.json()
+        print(len)
 
 loop.run_until_complete(main())
 ```
--- async_client_decorator/__init__.py
+++ async_client_decorator/__init__.py
@@ -1,6 +1,57 @@
+"""MIT License
+
+Copyright (c) 2023 gunyu1019
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from typing import NamedTuple, Literal, Optional
+
 from .body import Body
 from .form import Form
 from .header import Header
 from .query import Query
 from .request import request
 from .session import Session
+
+
+__title__ = "async_client_decorator"
+__author__ = "gunyu1019"
+__license__ = "MIT"
+__copyright__ = "Copyright 2023-present gunyu1019"
+__version__ = "0.1.0"  # version_info.to_string()
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    release_level: Optional[Literal["alpha", "beta", "candidate", "final"]]
+    serial: int
+
+    def to_string(self) -> str:
+        _version_info = f"{self.major}.{self.minor}.{self.micro}"
+        if self.release_level is not None:
+            _version_info += "-{}".format(self.release_level) + str(self.serial)
+        return _version_info
+
+
+version_info: VersionInfo = VersionInfo(
+    major=0, minor=6, micro=0, release_level=None, serial=0
+)
