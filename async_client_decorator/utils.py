fb3352d130dca148816da349154efed5247df9f4
gunyu1019 <gunyu1019@gmail.com>
2024-01-31 22:02:18 +0900

    [Feat] Add make_collection and get_origin_for_generic method

--- async_client_decorator/utils.py
+++ async_client_decorator/utils.py
@@ -1,5 +1,5 @@
 from collections.abc import Collection
-from types import UnionType
+from types import UnionType, GenericAlias
 from typing import Annotated, get_origin
 
 
@@ -8,8 +8,7 @@ def is_subclass_safe(_class, _class_info) -> bool:
     Same functionality as `issubclass` method
     However, _class parameter can be list of type.
     """
-    if not isinstance(_class, Collection):
-        _class = (_class,)
+    _class = make_collection(_class)
     return any([issubclass(t, _class_info) for t in _class if isinstance(t, type)])
 
 
@@ -28,3 +27,19 @@ def is_annotated_parameter(t) -> bool:
     Return `True` if type is Annotated
     """
     return get_origin(t) is Annotated
+
+
+def get_origin_for_generic(t):
+    """
+    If type is Generic, return origin of generic type
+    else return t
+    """
+    if isinstance(t, GenericAlias):
+        return t.__origin__
+    return t
+
+
+def make_collection(t):
+    if not isinstance(t, Collection):
+        return (t,)
+    return t
