ddfda07ed48f389f777d719de9808ba11573df46
gunyu1019 <gunyu1019@gmail.com>
2024-03-02 02:33:29 +0900

    [Feat] #7 - Add before_request and after_request to hook

--- async_client_decorator/session.py
+++ async_client_decorator/session.py
@@ -20,37 +20,57 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
 from __future__ import annotations
 
 import asyncio
-import aiohttp
 import functools
+import inspect
+from typing import TYPE_CHECKING, Optional, Any
 
-from typing import TYPE_CHECKING, Optional
-from ._types import RequestFunction
+import aiohttp
+
+from .request import RequestCore
 
 if TYPE_CHECKING:
     from typing_extensions import Self
     from types import TracebackType
 
+    from ._types import RequestFunction
+
 
 class Session:
     """A class to manage session for managing decoration functions."""
-
-    def __init__(self, base_url: str, directly_response: bool = False, **kwargs):
+    def __init__(
+            self,
+            base_url: str,
+            *,
+            directly_response: bool = False,
+            loop: asyncio.AbstractEventLoop = None,
+            _is_single_session: bool = False,
+            **kwargs
+    ):
         self.directly_response = directly_response
         self.base_url = base_url
+        self.loop = loop
 
-        self.session = aiohttp.ClientSession(self.base_url, **kwargs)
+        self.session = aiohttp.ClientSession(self.base_url, loop=self.loop, **kwargs)
+
+        if not _is_single_session:
+            for name, func in inspect.getmembers(self):
+                if not isinstance(func, RequestCore):
+                    continue
+
+                func.session = self
 
     async def __aenter__(self) -> Self:
         return self
 
     async def __aexit__(
-            self,
-            exc_type: Optional[type[BaseException]],
-            exc_val: Optional[BaseException],
-            exc_tb: Optional[TracebackType]
+        self,
+        exc_type: Optional[type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
     ):
         await self.close()
 
@@ -76,9 +96,23 @@ class Session:
     async def delete(self, path: str, **kwargs):
         return await self.session.delete(path, **kwargs)
 
+    async def _make_request(self, request: RequestCore, path: str, **kwargs):
+        _req_obj, _path = await self.before_request(request, path)
+        request_kwargs = _req_obj.get_request_kwargs()
+        return await self.session.request(_req_obj.method, _path, **request_kwargs)
+
+    async def before_request(self, request: RequestCore, path: str) -> RequestCore:
+        return request, path
+
+    async def after_request(self, response: aiohttp.ClientResponse) -> Any:
+        return response
+
     @classmethod
     def single_session(
-        cls, base_url: str, loop: asyncio.AbstractEventLoop = None, **session_kwargs
+            cls,
+            base_url: str,
+            loop: asyncio.AbstractEventLoop = None,
+            **session_kwargs
     ):
         """A single session for one request.
 
@@ -102,17 +136,18 @@ class Session:
         """
 
         def decorator(func: RequestFunction):
-            if not asyncio.iscoroutinefunction(func):
-                raise TypeError("function %s must be coroutine.".format(func.__name__))
-
             @functools.wraps(func)
             async def wrapper(*args, **kwargs):
-                client = cls(base_url, loop, **session_kwargs)
-                response = await func(client, *args, **kwargs)
+                client = cls(base_url, loop=loop, _is_single_session=True, **session_kwargs)
+                func.session = client
+                response = await func(*args, **kwargs)
                 if not client.closed:
                     await client.close()
                 return response
 
+            wrapper.__core__ = func
+            wrapper.before_hook = func.before_hook
+            wrapper.after_hook = func.after_hook
             return wrapper
 
         return decorator
