bbd6eab36abe8d372ae25dfa414e9b3663eeee68
gunyu1019 <gunyu1019@gmail.com>
2024-03-05 20:57:58 +0900

    [Deploy] Stable released (v1.0.0)

--- ahttp_client/__init__.py
+++ ahttp_client/__init__.py
@@ -36,7 +36,7 @@ __title__ = "ahttp_client"
 __author__ = "gunyu1019"
 __license__ = "MIT"
 __copyright__ = "Copyright 2023-present gunyu1019"
-__version__ = "1.0.0-beta"  # version_info.to_string()
+__version__ = "1.0.0"  # version_info.to_string()
 
 
 class VersionInfo(NamedTuple):
