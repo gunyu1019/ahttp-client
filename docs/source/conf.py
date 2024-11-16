d49fdbbaf53877691d265d2fe76c7b8ec18a133d
gunyu1019 <gunyu1019@gmail.com>
2024-11-16 07:42:26 +0000

    [Docs] #15 - Add configuration of Sphinx documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,15 +14,36 @@ release = 'v1.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    'sphinx.ext.intersphinx',
+]
+add_module_names = False
+
+intersphinx_mapping = {
+  'py': ('https://docs.python.org/3', None),
+  'aio': ('https://docs.aiohttp.org/en/stable/', None),
+  'pydantic': ('https://docs.pydantic.dev/latest/', None)
+}
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+language = 'en'
 
+locale_dirs = ['locale/']
+gettext_compact = False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinxawesome_theme'
 html_static_path = ['_static']
+html_css_files = ["custom_theme.css"]
+
+## Template Option
+html_title = "ahttp_client"
+html_sidebars = {
+  "**": ["sidebar_main_nav_links.html", "global_sidebar_toc.html"]
+}
