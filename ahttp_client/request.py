ea0195fd417995b1744fcfa3690e1ad7c96f7031
gunyu1019 <gunyu1019@gmail.com>
2026-03-11 00:58:35 +0900

    [Fix] Correct typo in variable name from 'intace_origin' to 'instance_origin' in request.py

--- ahttp_client/request.py
+++ ahttp_client/request.py
@@ -404,7 +404,7 @@ class RequestCore:
                     component_type = annotation
                     break
 
-            intace_origin = [get_origin_for_generic(t) for t in make_collection(separated_origin)]
+            instance_origin = [get_origin_for_generic(t) for t in make_collection(separated_origin)]
 
             if issubclass(component_type, Header) or parameter.name in header_parameter:
                 name = self._get_component_name(parameter.name, component_instance)
@@ -428,7 +428,7 @@ class RequestCore:
                 self._duplicated_check_body()
             elif issubclass(component_type, Body) or parameter.name == body_parameter:
                 self._duplicated_check_body_parameter(True)
-                if is_subclass_safe(intace_origin, Collection):
+                if is_subclass_safe(instance_origin, Collection):
                     self.body_parameter_type = "json"
                 else:
                     self.body_parameter_type = "data"
@@ -436,7 +436,7 @@ class RequestCore:
                 self._duplicated_check_body_parameter()
                 self._duplicated_check_body()
             elif issubclass(component_type, aiohttp.ClientResponse) or is_subclass_safe(
-                intace_origin, aiohttp.ClientResponse
+                    instance_origin, aiohttp.ClientResponse
             ):
                 self.response_parameter.append(parameter.name)
 
