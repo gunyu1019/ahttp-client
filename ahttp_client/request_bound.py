517d75fb25fddb13e55da9a08c049ea3a224cb6a
gunyu1019 <gunyu1019@gmail.com>
2026-07-16 00:56:20 +0900

    [Feat] #28 - Add RequestBound, RequestAsyncBound, and RequestSyncBound classes for improved session handling

---
+++ ahttp_client/request_bound.py
@@ -0,0 +1,62 @@
+"""MIT License
+
+Copyright (c) 2023-present gunyu1019
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any
+    from .session import BaseSession, AsyncSession, Session
+    from .request import RequestCore, AsyncRequestCore, SyncRequestCore
+
+
+class RequestBound(ABC):
+    def __init__(self, core: RequestCore, session: BaseSession):
+        self._core = core
+        self._session = session
+
+    @property
+    def __core__(self) -> RequestCore:
+        return self._core
+
+    @abstractmethod
+    def __call__(self, *args, **kwargs) -> Any:
+        pass
+
+
+class RequestAsyncBound(RequestBound):
+    _session: AsyncSession
+    _core: AsyncRequestCore
+
+    async def __call__(self, *args, **kwargs) -> Any:
+        return await self._core._execute(self._session, *args, **kwargs)
+
+
+class RequestSyncBound(RequestBound):
+    _session: Session
+    _core: SyncRequestCore
+
+    def __call__(self, *args, **kwargs) -> Any:
+        return self._core._execute(self._session, *args, **kwargs)
