80b856d1971fb0d9479218b8ad44ed139b0c225b
gunyu1019 <gunyu1019@gmail.com>
2026-07-17 02:32:03 +0900

    [Feat] Add serializer and deserializer decorators for model handling

---
+++ ahttp_client/serializer.py
@@ -0,0 +1,52 @@
+from typing import Optional
+from functools import wraps
+
+from ._types import RequestDecorator
+from .request import RequestCore, AsyncRequestCore, SyncRequestCore
+from .serialization.base import BaseSerializer, BaseDeserializer, ModelT
+
+
+def serialize(
+        model: Optional[ModelT] = None,
+        **serializer_kwargs,
+):
+    model_cls = BaseSerializer.from_model(model, **serializer_kwargs)
+    if model is None and len(serializer_kwargs) > 0:
+        model_cls = BaseSerializer.late_bind(**serializer_kwargs)
+
+    if model is None:
+        raise TypeError(f"No serializer found for {model}")
+
+    def decorator(func: RequestDecorator[AsyncRequestCore, SyncRequestCore] | RequestCore):
+        if isinstance(func, RequestCore):
+            func._serializer = model_cls
+            return func
+        if not hasattr(func, "__extension__"):
+            func.__extension__ = dict()
+        func.__extension__["serializer"] = model_cls
+        return func
+
+    return decorator
+
+
+def deserialize(
+        model: Optional[ModelT] = None,
+        **deserializer_kwargs,
+):
+    model_cls = BaseDeserializer.from_model(model, **deserializer_kwargs)
+    if model is None and len(deserializer_kwargs) > 0:
+        model_cls = BaseDeserializer.late_bind(**deserializer_kwargs)
+
+    if model is None:
+        raise TypeError(f"No deserializer found for {model}")
+
+    def decorator(func: RequestDecorator[AsyncRequestCore, SyncRequestCore] | RequestCore):
+        if isinstance(func, RequestCore):
+            func._deserializer = model_cls
+            return func
+        if not hasattr(func, "__extension__"):
+            func.__extension__ = dict()
+        func.__extension__["deserializer"] = model_cls
+        return func
+
+    return decorator
